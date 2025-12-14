ccc9551a541a4194165f852a7238f4694af72c01
arif <arif@ASUS>
2025-12-14 17:57:44 +0600

    finallabtask1

---
+++ finallab1/Finaltask2.3.php
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+<?php
+    $number = 7;
+
+    if ($number % 2 == 0) {
+        echo "<p>The number " . $number . " is Even.</p>";
+    } else {
+        echo "<p>The number " . $number . " is Odd.</p>";
+    }
+?>
+=======
+<?php
+    $number = 7;
+
+    if ($number % 2 == 0) {
+        echo "<p>The number " . $number . " is Even.</p>";
+    } else {
+        echo "<p>The number " . $number . " is Odd.</p>";
+    }
+?>
+>>>>>>> f3ded1bb21eda0600b494823ebe6cd1e3e9a0370
---
+++ finallab1/Finaltask2.6.php
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+<?php
+    $arr = [12, 35, 7, 56, 23, 19, 44];
+    $searchElement = 23;
+    if (in_array($searchElement, $arr)) {
+        echo "<p>The element " . $searchElement . " was found in the array.</p>";
+    } else {
+        echo "<p>The element " . $searchElement . " was not found in the array.</p>";
+    }
+?>
+=======
+<?php
+    $arr = [12, 35, 7, 56, 23, 19, 44];
+    $searchElement = 23;
+    if (in_array($searchElement, $arr)) {
+        echo "<p>The element " . $searchElement . " was found in the array.</p>";
+    } else {
+        echo "<p>The element " . $searchElement . " was not found in the array.</p>";
+    }
+?>
+>>>>>>> f3ded1bb21eda0600b494823ebe6cd1e3e9a0370
